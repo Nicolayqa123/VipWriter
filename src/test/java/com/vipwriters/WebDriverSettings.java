96f5eff5f2b9e611d7de93dc4f1a916033fd8b15
Nicolayqa123 <nicolayqa@gmail.com>
2018-06-26 12:52:34 +0300

    test3

--- src/test/java/com/vipwriters/WebDriverSettings.java
+++ src/test/java/com/vipwriters/WebDriverSettings.java
@@ -62,35 +62,6 @@ public void close() {
     driver.quit();
 }
 
-    public class CustomTestListener extends TestListenerAdapter {
-
-        private Logger log = LoggerFactory.getLogger(CustomTestListener.class);
-
-        @Override
-        public void onTestStart(ITestResult result) {
-            log.info("Test class started: " + result.getTestClass().getName());
-            log.info("Test started: " + result.getName());
-        }
-
-        @Override
-        public void onTestSuccess(ITestResult result) {
-            log.info("Test SUCCESS: " + result.getName());
-        }
-
-        @Override
-        public void onTestFailure(ITestResult result) {
-            makeScreenshot();
-            log.info("Test FAILED: " + result.getName());
-            if (result.getThrowable()!=null) {
-                result.getThrowable().printStackTrace();
-            }
-        }
-
-        @Attachment(value = "Page screenshot", type = "image/png")
-        private byte[] makeScreenshot() {
-            return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        }
-    }
 
 
 
