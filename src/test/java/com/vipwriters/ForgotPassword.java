6b9ac088b1582d380ef1711590d245c3c05a9d3e
Nicolayqa123 <nicolayqa@gmail.com>
2018-07-19 12:35:55 +0300

    78

--- src/test/java/com/vipwriters/ForgotPassword.java
+++ src/test/java/com/vipwriters/ForgotPassword.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class ForgotPassword extends WebDriverSettings {
 
@@ -22,35 +23,31 @@ public class ForgotPassword extends WebDriverSettings {
         Lending.forgotPasswordSend(driver).click();
 
         assertEquals("The password reset link was sent to you via email. Please navigate it to change your password.\n" +
-                "If the email does not arrive, please contact support administrator.", driver.findElement(By.cssSelector("#loginForm > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)")).getText());
+                "If the email does not arrive, please contact support administrator.", driver.findElement(By.xpath("//*[@id=\"sendResetPasswordEmailForm\"]/p")).getText());
     }
 
-
+@Test
     public void ForgotPasswordNoValid() throws Exception{
 
-        driver.get("https://client.urgentpapers.org/");
+        driver.get("https://writer.urgentpapers.org");
+        TimeUnit.SECONDS.sleep(3);
+        Lending.loginForm(driver).click();
+        Lending.forgotPassword(driver).click();
+        Lending.forgotPasswordMail(driver).sendKeys("test@@..com");
+        Lending.forgotPasswordSend(driver).click();
 
-        driver.findElement(loginClient).click();
-        driver.findElement(resetPasswordClient).click();
-            TimeUnit.SECONDS.sleep(3);
-        driver.findElement(resetPasswordCMail).sendKeys("Test@test.com");
-        driver.findElement(resetPasswordCSend).click();
-            TimeUnit.SECONDS.sleep(3);
-        assertEquals("The email address is invalid", driver.findElement(By.xpath("//*[@id=\"sendResetPasswordEmailForm\"]/div[2]/p")).getText());
+        assertEquals("Enter valid email.", driver.findElement(By.xpath("//*[@id=\"sendResetPasswordEmailForm\"]/div[2]/div[1]/p")).getText());
     }
-
+@Test
     public void ForgotPasswordEmptyFields()throws Exception {
 
-        driver.get("https://client.urgentpapers.org/");
-            TimeUnit.SECONDS.sleep(5);
-        WebElement login =  driver.findElement(By.xpath("/html/body/div/div/div/div[1]/header/div/div[2]/nav/ul/li[10]/a"));
-        WebElement forgotbutton = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div/div/form/div/div[5]/a"));
-        login.click();
-        forgotbutton.click();
-            TimeUnit.SECONDS.sleep(3);
-        driver.findElement(By.xpath("//*[@id=\"sendResetPasswordEmail\"]")).click();
-            TimeUnit.SECONDS.sleep(3);
-        assertEquals("The user's email is required", driver.findElement(By.xpath("//*[@id=\"sendResetPasswordEmailForm\"]/div[2]/p")).getText());
+        driver.get("https://writer.urgentpapers.org");
+        TimeUnit.SECONDS.sleep(3);
+        Lending.loginForm(driver).click();
+        Lending.forgotPassword(driver).click();
+        Lending.forgotPasswordMail(driver).sendKeys("");
+        Lending.forgotPasswordSend(driver).click();
+        assertEquals("Email cannot be empty.", driver.findElement(By.xpath("//*[@id=\"sendResetPasswordEmailForm\"]/div[2]/div[1]/p")).getText());
     }
 
 
