ac8ed69b8b8d8767853009fb8973ebfd6fd3e31c
Nicolayqa <nicolayqa@gmail.com>
2019-03-22 12:44:59 +0200

    65656565465

--- src/test/java/com/vipwriters/MyOrders/MyOrdersDownloadFilesTest.java
+++ src/test/java/com/vipwriters/MyOrders/MyOrdersDownloadFilesTest.java
@@ -9,6 +9,15 @@ import org.junit.Test;
 import java.util.concurrent.TimeUnit;
 
 public class MyOrdersDownloadFilesTest extends WebDriverSettings {
+    @Test
+    public void Download () throws Exception {
+        WritersLogin();
+        TimeUnit.SECONDS.sleep(4);
+        MyOrders.myOrders(driver).click();
+        MyOrders.OrderInProgress(driver).click();
+        TimeUnit.SECONDS.sleep(2);
+        DetailedOrder.downloadFile(driver).click();
 
 
+    }
 }
