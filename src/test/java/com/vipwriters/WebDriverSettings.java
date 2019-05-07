5970f181945ed4446e16b641cb32d469f1fac767
Nicolayqa <nicolayqa@gmail.com>
2019-05-07 16:52:11 +0300

    lkoj

--- src/test/java/com/vipwriters/WebDriverSettings.java
+++ src/test/java/com/vipwriters/WebDriverSettings.java
@@ -154,8 +154,8 @@ public class WebDriverSettings  {
            // System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
             DesiredCapabilities capability = DesiredCapabilities.firefox();
             capability.setBrowserName("firefox");
-            capability.setPlatform(Platform.LINUX);
-            capability.setVersion("");
+           // capability.setPlatform(Platform.LINUX);
+          //  capability.setVersion("");
           //  System.setProperty("webdriver.gecko.driver", "driver/geckodriver");
            // capability.setCapability("webdriver.gecko.driver", "driver/geckodriver");
            // driver = new RemoteWebDriver(new URL("http://localhost:8081/wd/hub"), capability);
@@ -180,10 +180,10 @@ public class WebDriverSettings  {
 
 
 
-            driver = new FirefoxDriver(capability);
+            driver = new FirefoxDriver();
           //  System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
             // For Linux
-            System.setProperty("webdriver.gecko.driver", "/var/jenkins_home/workspace/Tests/driver/geckodriver");
+            System.setProperty("webdriver.gecko.driver", "//test2/driver/geckodriver");
       //      System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         //      driver = new ChromeDriver();
         //    System.setProperty("webdriver.ie.driver", "C://Programms/IEDriverServer.exe");
