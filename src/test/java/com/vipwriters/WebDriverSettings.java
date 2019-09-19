f53a2241b7cc37e331a97aece2fccc62b686d9f0
Nicolayqa <nicolayqa@gmail.com>
2019-09-19 10:58:27 +0300

    test

--- src/test/java/com/vipwriters/WebDriverSettings.java
+++ src/test/java/com/vipwriters/WebDriverSettings.java
@@ -481,10 +481,10 @@ public class WebDriverSettings   {
 
         String[] subjectChoose = {"Architecture", "Art history", "Aviation", "Classic English literature", "Culture", "Drama", "Film studies", "Literature", "Music", "Philosophy", "Poetry", "Religious studies", "Shakespeare studies", "Theater studies", "World literature", "Business", "Human Resource Management", "Investments", "Management", "Marketing", "C++", "Communication", "Java programming", "Linear programming", "Programming", "Accounting", "Economics", "Finance", "Engineering", "Geology", "Technology", "History", "Political science", "World affairs", "English", "Criminal Law", "Law", "Anthropology", "Astronomy", "Biochemistry", "Biology", "Chemistry", "Ecology", "Environmental studies", "Genetics", "Geography", "Medicine", "Nursing", "Physics", "Mathematics", "MATLAB", "SPSS", "Statistics", "Civics", "Cognitive Science", "Criminology", "Education", "Family and consumer science", "Health", "Journalism", "Psychology", "Sociology", "Women and gender studies",
         };
-        String subjectList = subjectChoose[va];
+      //  String subjectList = subjectChoose[va];
         String[] paper_typeChoose = {"Admission essay", "Article review", "Article", "Dissertation proposal", "Dissertation", "Essay", "Research paper", "Research proposal", "Term paper", "Thesis", "Other", "Business plan", "Business feasibility plan", "Investor business plan", "One-page business plan", "Operations business plan", "Start-up Business Plan", "Scholarship essay", "Admission essay", "Application letter", "Personal statement", "Business School application essay", "Graduate School application essay", "Law School application essay", "MBA admission essay", "MBA application essay", "Medical School application essay", "Study abroad application essay", "Cover letter", "Military cover letter", "Referral cover letter", "Cold-contact cover letter", "Entry level cover letter", "Executive cover letter", "Internship cover letter", "CV", "Academic CV", "Executive CV", "Internship CV", "Military CV", "Dissertation", "Dissertation abstract", "Dissertation chapter", "Dissertation conclusion", "Dissertation hypothesis", "Dissertation introduction", "Dissertation methodology", "Dissertation proposal", "Dissertation results", "Dissertation discussion", "Literature review", "Annotated bibliography", "Argumentative essay", "Course work", "Creative writing", "Essay", "Literature review", "Presentation", "Report", "OtherEssay", "Analytical essay", "Cause and Effect essay", "Comparative essay", "Compare and contrast essay", "Creative writing essay", "Deductive essay", "Definition essay", "Descriptive essay", "Exploratory essay", "Expository essay", "Informal essay", "Literary essay", "Literature essay", "MBA essay", "Military essay", "Narrative essay", "Persuasive essay", "Reflective essay", "Response essay", "Short essay", "Algebra homework help", "College homework help", "English homework help", "Homework for kids", "Maths homework help", "School homework help (middle & high)", "Science homework help", "Article", "Biography", "Case study", "Critical thinking", "Term paper", "Multiple choice questions", "Annotated bibliography", "Book critique", "Capstone project", "Coursework", "Critical analysis", "Grant proposal", "Journal critique", "Powerpoint presentation", "Problem solution", "Reaction paper", "Speech", "Other", "Problem solving", "Lab report", "Business report", "Engineering report", "Experience report", "Feasibility report", "Formal report", "Investigation report", "Policy report", "Progress report", "Research report", "Science report", "Short report", "Technical report", "Workplace report", "Research paper", "Research proposal", "Research thesis", "Research outline", "Research summary", "Resume", "Chronological resume", "Combination (combined) resume", "Entry level resume", "Functional resume", "Internship resume", "Military resume", "Article review", "Book review", "Movie review", "Critical appraisal", "Critical review", "Meta-analysis", "Peer review", "Systematic review", "Thesis", "Thesis proposal", "Thesis statement", "Analytical thesis statement", "Project thesis", "Thesis abstract", "Thesis conclusion", "Thesis discussion", "Thesis introduction", "Thesis methodology",
         };
-        String paper_typeList = paper_typeChoose[vb];
+      //  String paper_typeList = paper_typeChoose[vb];
 
 
 
@@ -493,11 +493,11 @@ public class WebDriverSettings   {
         driver.findElementByCssSelector(".btn-success").click();
         // driver.get("https://Pro-Papers.com/order");
         //  Lending.Order_Now(driver).click();
-        Registered.paper_type(driver).sendKeys(paper_typeList);
-        Registered.subject(driver).sendKeys(subjectList);
+        Registered.paper_type(driver).sendKeys(paper_typeChoose[va]);
+        Registered.subject(driver).sendKeys(subjectChoose[vb]);
         Registered.topic(driver).sendKeys("Test order Test");
-        Select paper_format = new Select(driver.findElement(By.name("paper_format")));
-        paper_format.selectByValue(String.valueOf(z));
+       // Select paper_format = new Select(driver.findElement(By.name("paper_format")));
+       // paper_format.selectByValue(String.valueOf(z));
 //      Registered.number_of_references(driver).sendKeys("" + b);
         Registered.paper_details(driver).sendKeys("Test test test tes tes ");
         Registered.nextStep(driver).click();
